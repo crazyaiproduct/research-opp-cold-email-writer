80c4a64321913e7f5df884c3492ac7a6615af31a
jingyanjiang <jingyanjiang98@outlook.com>
2024-06-29 19:52:07 -0400

    Bug fixed.

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def submit():
     # if request.method == 'POST':
     input_dict = dict()
     input_dict['Professor_Name'] = request.form['professor'],
-    input_dict['Opptunity_Type'] = request.form['main_opp'],
+    input_dict['Opportunity_Type'] = request.form['main_opp'],
     input_dict['Semester'] = request.form['semester'],
     input_dict['Alternate_Opportunity_Type'] = request.form['second_opp'],
     input_dict['Your_Name'] = request.form['name'],
@@ -24,16 +24,17 @@ def submit():
     input_dict['Personalities_Strength'] = request.form['strength'],
     input_dict['Related_Experiences'] = request.form['experience'],
     input_dict['Future_Resolution_Goal'] = request.form['goal'],
-    input_dict['Professor_Website'] = request.form['professor'],
-    input_dict['Professor_Research_Interest'] = request.form['professor'],
-    input_dict['Interest_Paper_URL'] = request.form['professor'],
-    input_dict['Paper_Abstract_CP'] = request.form['professor'],
-    input_dict['Documents'] = request.form['professor']
+    input_dict['Professor_Website'] = request.form['pro_web'],
+    input_dict['Professor_Research_Interest'] = request.form['pro-research-domain'],
+    input_dict['Interest_Paper_URL'] = request.form['paper_url'],
+    input_dict['Paper_Abstract_CP'] = request.form['paper_abstract'],
+    input_dict['Documents'] = request.form['doc']
 
     # Get the email
     payload = em.process_input(input_dict=input_dict)
     response_message = em.call_llm(payload=payload)
-    return render_template('feedback.html', response_message=response_message)
+    parsed_resp = em.parse_response(response_message)
+    return render_template('feedback.html', response_message=parsed_resp)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- email_model.py
+++ email_model.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import time
+import re
 
 class EmailModel():
     def __init__(self) -> None:
@@ -8,11 +9,12 @@ class EmailModel():
         self.headers = {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'Authorization': 'Bearer sk-4ce428b32cc74ee39029bb69fc3144f4' # Replace with your own api key here
+            'Authorization': 'Bearer [API Key]' # Replace [API Key] with your own api key here
         }
     
     def process_input(self, input_dict):
-        payload = '''
+        
+        pay_load_string = '''
                     <Instructions>
                     You will be writing an email to a professor {Professor_Name} to express interest in research opportunities. I will
                     provide you with background context and specific instructions for what to include in each paragraph
@@ -21,7 +23,7 @@ class EmailModel():
                     Here is the background context you should refer to when writing the email:
                     <context>
                     I am interested in the research of Professor {Professor_Name} and hope to work with him. 
-                    I would like to write an email to reach out to Professor {Professor_Name} asking for {Opptunity_Type} opportunities starting in the {Semester}, and {Alternate_Opportunity_Type} opportunities starting as soon as possible.
+                    I would like to write an email to reach out to Professor {Professor_Name} asking for {Opportunity_Type} opportunities starting in the {Semester}, and {Alternate_Opportunity_Type} opportunities starting as soon as possible.
                     Also, please explain why my past experience makes me a good fit for the opportunity in the email.
 
                     <student_info>
@@ -138,7 +140,7 @@ class EmailModel():
                     salutation, and include a sign-off at the end.
                     '''.format(
                         Professor_Name=input_dict['Professor_Name'],
-                        Opptunity_Type=input_dict['Opptunity_Type'],
+                        Opportunity_Type=input_dict['Opportunity_Type'],
                         Semester=input_dict['Semester'],
                         Alternate_Opportunity_Type=input_dict['Alternate_Opportunity_Type'],
                         Your_Name=input_dict['Your_Name'],
@@ -155,6 +157,26 @@ class EmailModel():
                         Paper_Abstract_CP=input_dict['Paper_Abstract_CP'],
                         Documents=input_dict['Documents']
                     )
+        
+        payload = json.dumps({
+            "messages":[
+                {
+                    "role": "user",
+                    'content': pay_load_string
+                }
+            ],
+            "model": "deepseek-coder",
+            "frequency_penalty": 0,
+            "max_tokens": 1024,
+            "presence_penalty": 0,
+            "stop": None,
+            "stream": False,
+            "temperature": 0.2,
+            "top_p": 1,
+            "logprobs": False,
+            "top_logprobs": None
+                    })
+
         return payload
 
 
@@ -162,8 +184,20 @@ class EmailModel():
     def call_llm(self, payload):
         start = time.time()
         response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        print("Response: ", response.text)
         json_response = json.loads(response.text)
+        
         end = time.time()
         print("Backend Response Time (s): ", end - start)
         return json_response["choices"][0]["message"]["content"]
+
+
+    def parse_response(self, response):
+        response = response.replace("<email>", "")
+        response = response.replace("</email>", "")
+        response_list =  re.split(r'<\/?paragraph>|\n', response)
+        parsed_resp = "<br><br>".join([word for word in response_list if word])
+        
+        return parsed_resp
+
         
